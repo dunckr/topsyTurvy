bacb33aff35597bc50b9da903fe70d4adab5dca8
duncky <d@duncky.com>
2013-12-08 13:02:11 +0000

    add css injection, css rotating

--- app/scripts/topsyTurvy.js
+++ app/scripts/topsyTurvy.js
@@ -1,5 +1,27 @@
 'use strict';
 (function ($) {
   $(document).ready(function(){
+    var minTimeRange = 1000*60;
+    var maxTimeRange = 1000*60*10;
+
+    setInterval(function() {
+      injectCSS();
+    }, randomise(minTimeRange,maxTimeRange));
   });
+
+  function injectCSS() {
+    var path = chrome.extension.getURL('styles/main.css');
+    inject(path);
+  }
+
+  function inject(path) {
+    $('head').append($('<link>')
+      .attr('rel','stylesheet')
+      .attr('type','text/css')
+      .attr('href', path));
+  }
+
+  function randomise(max,min) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 })(jQuery);
